c6a91031856feccfd7951db680e0d8eedfb2902d
هانيااحمدسامىعبدالعظ <@DESKTOP-E8F3FRI>
2022-06-06 23:46:23 +0200

    ..

--- Logoutdemo.py
+++ Logoutdemo.py
@@ -1,6 +1,7 @@
 import mysql.connector 
 from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'secret key'
@@ -12,6 +13,7 @@ mydb = mysql.connector.connect(
     password="root",
     database="obgyn2"
 )
+
 
 mycursor = mydb.cursor(buffered = True)
 
@@ -136,16 +138,12 @@ def signup():
                     result = mycursor.fetchone()
                     patient_id=result[0]
                     session['id']=patient_id
-                    mycursor.execute(
-                        'INSERT INTO patient (patient_id,patient_first_name,patient_last_name,patient_email_address,patient_age,patient_phone_no) VALUES( %s, %s,%s, %s, %s, %s)', (patient_id, first_name, last_name, e_mail, age, phone_number))
+                    mycursor.execute('INSERT INTO patient (patient_id,patient_password,patient_first_name,patient_last_name,patient_email_address,patient_age,patient_phone_no) VALUES( %s, %s,%s, %s, %s, %s,%s)', (patient_id, password,first_name, last_name, e_mail, age, phone_number,))
                     mydb.commit()
                     
                     return render_template('indexpatient.html', user_name=username, pass_word=password)
 
         return render_template('signup.html')
-       # else:
-        #if error_statement =='no_error':  
-          # return render_template('signup.html', user_name=username, passwordd=password)
 
 @app.route('/login', methods=["GET", "POST"])
 def signin():
@@ -190,17 +188,6 @@ def logout():
     session["role"]= None
     return redirect("/")
 
-''' @app.route('/doctor/myappointments',methods=['GET','POST'])
-def my_appointments():
-    if session['role'] != "doctor":
-        redirect('/')
-    else:    
-        mycursor.execute('SELECT patient_id, booked_appointment_time, booked_appointment_date, doctor_comment FROM booked_appointments WHERE doctor_id = %d AND , past_or_upcoming = "past"',(session["id"]))
-        past=tuple(mycursor.fetchall())
-        mycursor.execute('SELECT patient_id, booked_appointment_time, booked_appointment_date, doctor_comment FROM booked_appointments WHERE doctor_id = %d AND , past_or_upcoming = "upcoming"',(session.id))
-        upcoming=tuple(mycursor.fetchall())
-        return render_template("myappointments.html", past_appointments=past, upcoming_appointments=upcoming)
- '''
 @app.route('/doctor/myappointments',methods=['GET','POST'])
 def my_appointments():
     mycursor.execute('SELECT patient_id, booked_appointment_date, booked_appointment_time, doctor_comment FROM booked_appointment WHERE doctor_id = %s',(session["id"]))
@@ -220,43 +207,68 @@ def admin_doctors_appointments():
         header= ("Patient ID","Doctor ID", "Appointment Date", "Appointment Time", "Doctor's Comment")
         return render_template("allappointments.html",past_appointments=past, upcoming_appointments=upcoming, head= header)
 
-@app.route('/patient/availableappointments', methods= ['GET','POST'])
+@app.route('/patient/availableappointments')
 def avaialable_appointments():
-    #return l hanya el header AND rows ofc
+    
     if session['role']== "patient":
-        mycursor.execute('')
         mycursor.execute('SELECT doctor_name, available_appointment_date, available_appointment_time FROM doctor INNER JOIN available_appointment USING (doctor_id)')
         data=tuple(mycursor.fetchall())
+        mycursor.execute('SELECT doctor_name FROM doctor INNER JOIN available_appointment USING (doctor_id)')
+        doctors_list=mycursor.fetchall()
+        doctors=[]
+        for dr in doctors_list:
+            doctors.append(dr[0])
+        doctors=tuple(doctors)
+        mycursor.execute('SELECT available_appointment_date FROM available_appointment INNER JOIN doctor USING (doctor_id)')
+        dates=mycursor.fetchall()
+        strings_of_dates=[]
+        for date in dates:
+            extract_date=date[0]
+            strings_of_dates.append(extract_date.strftime("%Y-%m-%d"))
+        times=tuple(mycursor.fetchall())
+        new_dates=tuple(strings_of_dates)
+        mycursor.execute('SELECT available_appointment_time FROM available_appointment INNER JOIN doctor USING (doctor_id)')
+        times_list=mycursor.fetchall()
+        times=[]
+        for dr in times_list:
+            times.append(dr[0])
+        times=tuple(times)
+        print(date, type(date))
         header= ("Doctor Name", "Appointment Date","Appointment Time")
-        return render_template("availableappointments.html",availableappointments=data, head= header)
+        return render_template("availableappointments.html",doctors=doctors,availableappointments=data, head= header, dates=new_dates,times=times)
     elif session['role']=='doctor':
         return render_template("indexdoctor.html")
     elif session['role']=='admin':
         return render_template("indexadmin.html")
-    else:
-        return redirect('/')   
+    return redirect('/')
     
-@app.route('/patient/availableappointments/patientbooking') 
-def booking():
-    return render_template("patientbooking.html") 
+    
+    
+    
 
-@app.route('/patient/availableappointments/patientbooking', methods= ["GET","POST"])    
+        
+
+
+@app.route('/patient/availableappointments', methods= ["GET","POST"])    
 def patient_booking():
     if session['role']== 'patient':
         if request.method == 'POST':
-            booked_date= request.form['datepicker']
+            booked_date= request.form['date']
             booked_time= request.form['time']
             dr= request.form['doctor']
             mycursor.execute('SELECT doctor_id FROM doctor WHERE doctor_name = %s', (dr,))
             doctorid = mycursor.fetchone()[0]
-            mycursor.execute('INSERT INTO booked_appointment (doctor_id, patient_id, booked_appointment_date, booked_appointment_time, past_or_upcoming) VALUES(%d, %d, %s, %s, "upcoming",)',(doctorid,session['id'][0],booked_date,booked_time,))
+            mycursor.execute('INSERT INTO booked_appointment (doctor_id, patient_id, booked_appointment_date, booked_appointment_time, past_or_upcoming) VALUES(%s, %s, %s, %s, "upcoming")',(doctorid,session['id'][0],booked_date,booked_time,))
             mydb.commit()
-            mycursor.execute('DELETE FROM available_appointment WHERE doctor_id= %d AND available_appointment_date =%s AND available_appointment_time=%s',(doctorid,booked_date,booked_time,))
+            mycursor.execute('DELETE FROM available_appointment WHERE doctor_id= %s AND available_appointment_date =%s AND available_appointment_time=%s',(doctorid,booked_date,booked_time,))
             mydb.commit()
-            mycursor.execute('SELECT booked_appointment_id FROM booked_appointment WHERE doctor_id=%d AND patient_id=%d AND booked_appointment_date=%s AND booked_appointment_time=%s AND past_or_upcoming = "upcoming"',(doctorid,session['id'][0],booked_date,booked_time,))
+            mycursor.execute('SELECT booked_appointment_id FROM booked_appointment WHERE doctor_id=%s AND patient_id=%s AND booked_appointment_date=%s AND booked_appointment_time=%s AND past_or_upcoming = "upcoming"',(doctorid,session['id'][0],booked_date,booked_time))
             app_id= mycursor.fetchone()[0]
             return render_template("bookingsuccess.html",dname=dr, appdate= booked_date, apptime=booked_time, appid=app_id)
-    else:
-        return redirect('/')
-if __name__ == '__main__':
+    elif session['role']=='doctor': 
+        return render_template('indexdoctor.html')
+    elif session['role']=='admin':
+        return render_template("indexadmin.html")
+    return redirect('/')
+if __name__ == '_main_':
     app.run(debug=True)
